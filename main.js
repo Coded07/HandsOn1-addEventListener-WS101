18a2ab4b34ee716001b2a3a15cd52c794a11a676
Coded07 <148201902+Coded07@users.noreply.github.com>
2024-01-21 11:39:31 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+btn1.addEventListener("click", function() {
+    alert("Button 1 was clicked!")
+});
+
+btn2.addEventListener("click", function() {
+    alert("Button 2 was clicked!")
+});
+
+btn3.addEventListener("click", function() {
+    alert("Button 3 was clicked!")
+});
